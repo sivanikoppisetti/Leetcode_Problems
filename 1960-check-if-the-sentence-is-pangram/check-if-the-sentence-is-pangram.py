b6fd4d117a1364155406bd9fdfe4df8f266191c2
sivanikoppisetti <sivanikoppisetti467@gmail.com>
2026-08-13 20:22:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (85.13%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        s = set(sentence)
+        if len(s) == 26:
+            return True
+        else:
+            return False
+        
