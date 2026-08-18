0195cd622f0c49926133bc615f97c5b65d68b48d
sivanikoppisetti <sivanikoppisetti467@gmail.com>
2026-08-18 20:50:36 +0530

    Time: 15 ms (60.69%) | Memory: 32.2 MB (51.84%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        set1 = set(nums)
+        if len(set1) < len(nums):
+            return True
+        else:
+            return False
+
+        
